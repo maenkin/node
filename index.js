41bffb76bd5ac4722af95d14d47df10c160fc04e
maenkin <rosdiyanto2022@gmail.com>
2022-06-30 00:20:25 +0700

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("hello apa kanbar"));
+app.get("/makan", (req, res) => res.send("udah makan?"));
+app.listen(port, () => console.log(`aplikasi berjalan di port ${port}`));
