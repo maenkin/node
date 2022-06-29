dfa1b14ff96b030ff2a1ca91d9361af487c4fe54
maenkin <rosdiyanto2022@gmail.com>
2022-06-30 00:23:41 +0700

    fix error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT | 3000;
 
 app.get("/", (req, res) => res.send("hello apa kanbar"));
 app.get("/makan", (req, res) => res.send("udah makan?"));
