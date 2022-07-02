3edb93180f61a27dfd47268d2f1504a870f7d4a9
maenkin <rosdiyanto2022@gmail.com>
2022-07-02 14:52:38 +0700

    Tambah keterangan

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Rosdiyanto ganteng dan menawan"));
+app.get("/", (req, res) => res.send("Anake bapak sunadi"));
 app.get("/makan", (req, res) => res.send("udah makan?"));
 app.listen(port, () => console.log(`aplikasi berjalan di port ${port}`));
